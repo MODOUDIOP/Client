d2df66fc92fc52ef43a3321245f718b3c04664f5
Modou DIOP <noreyni1133@gmail.com>
2023-02-23 16:43:41 +0000

    second commit

--- src/components/nav/Menu.js
+++ src/components/nav/Menu.js
@@ -1,4 +1,3 @@
-
 import { NavLink } from "react-router-dom";
 import { useAuth } from "../../context/auth";
 import { useNavigate } from "react-router-dom";
@@ -9,87 +8,86 @@ export default function Menu() {
   //hooks
   const [auth, setAuth] = useAuth();
   const [cart, setCart] = useCart();
-  
+
   const navigate = useNavigate();
 
   const logout = () => {
     setAuth({ ...auth, user: null, token: "" });
     localStorage.removeItem("auth");
     navigate("/");
-  }
+  };
   return (
     <>
-        
-      <ul className="nav d-flex justify-content-between shadow-sm mb-2 sticky-top bg-dark" >
+      <ul className="nav d-flex justify-content-between shadow-sm mb-2 sticky-top bg-dark">
         <li className="nav-item">
           <NavLink className="nav-link " aria-current="page" to="/">
             HOME
           </NavLink>
         </li>
 
-         <li className="nav-item">
+        <li className="nav-item">
           <NavLink className="nav-link " aria-current="page" to="/shop">
             SHOP
           </NavLink>
         </li>
         {/* <Search/>  */}
-        
-        <li className="nav-item mt-1">
-          <Badge count={cart?.length >= 1 ? cart.length : 0} offset={[-5,13]} showZero ="true">
-            <NavLink className="nav-link " aria-current="page" to="/cart">
-            CART
-          </NavLink>
 
+        <li className="nav-item mt-1">
+          <Badge
+            count={cart?.length >= 1 ? cart.length : 0}
+            offset={[-5, 13]}
+            showZero="true"
+          >
+            <NavLink className="nav-link " aria-current="page" to="/cart">
+              CART
+            </NavLink>
           </Badge>
         </li>
-      
 
         {!auth?.user ? (
-        <>
-          <li className="nav-item">
-          <NavLink className="nav-link" to="/login">
-            LOGIN
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/register">
-            REGISTER
-          </NavLink>
-        </li>
-        </>
+          <>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                LOGIN
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/register">
+                REGISTER
+              </NavLink>
+            </li>
+          </>
         ) : (
-            <div className="dropdown">
-              <li>
-                <a className="nav-link pointer dropdown-toggle" data-bs-toggle="dropdown">
-                  {auth?.user?.name?.toUpperCase()}
-                </a>
+          <div className="dropdown">
+            <li>
+              <a
+                className="nav-link pointer dropdown-toggle"
+                data-bs-toggle="dropdown"
+              >
+                {auth?.user?.name?.toUpperCase()}
+              </a>
 
-                <ul className="dropdown-menu ">
+              <ul className="dropdown-menu ">
                 <li>
-                  <NavLink className="nav-link" to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"}`}>
-                Dashboard
+                  <NavLink
+                    className="nav-link"
+                    to={`/dashboard/${
+                      auth?.user?.role === 1 ? "admin" : "user"
+                    }`}
+                  >
+                    Dashboard
                   </NavLink>
                 </li>
                 <li className="nav-item pointer">
                   <a onClick={logout} className="nav-link">
                     Logout
                   </a>
-        
-            </li>
+                </li>
               </ul>
-              </li>
-              
-              
-            </div>
-             
+            </li>
+          </div>
         )}
-        
-
-       
       </ul>
-    
- 
     </>
   );
-  
-  }
+}
